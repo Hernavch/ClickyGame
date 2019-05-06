d642e5681e5cef10e8765839154b6fe7da3d037f
Hernavch <hernandezvc1292@gmail.com>
2019-05-05 19:05:57 -0700

    Updates

---
+++ precache-manifest.030bdd5225d1745af237ad8d0722d804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a375fdc10fc5a1a5f5e203d15434d3d4",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "1bf788ade4e2cc3ce42c",
+    "url": "/ClickyGame/static/css/main.9a93368c.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/ClickyGame/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "1bf788ade4e2cc3ce42c",
+    "url": "/ClickyGame/static/js/main.d301956a.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
